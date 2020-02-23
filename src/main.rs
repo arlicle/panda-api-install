294a6e728a78e716ef6cf68fae859f8d6def86cb
edison <arlicle@gmail.com>
2020-02-24 07:51:47 +0800

    fix *

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@ fn main() {
     {
         println!("Reading some system info...");
         let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-        let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion")?;
-        let pf: String = cur_ver.get_value("ProgramFilesDir")?;
-        let dp: String = cur_ver.get_value("DevicePath")?;
+        let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion").unwrap();
+        let pf: String = cur_ver.get_value("ProgramFilesDir").unwrap();
+        let dp: String = cur_ver.get_value("DevicePath").unwrap();
         println!("ProgramFiles = {}\nDevicePath = {}", pf, dp);
-        let info = cur_ver.query_info()?;
+        let info = cur_ver.query_info().unwrap();
         println!("info = {:?}", info);
     }
 
