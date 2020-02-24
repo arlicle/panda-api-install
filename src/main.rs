9560dbe761d7feb72fdd3b4596c80e9a8abeb993
edison <arlicle@gmail.com>
2020-02-24 08:01:18 +0800

    fix *

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     {
         println!("Reading some system info...");
         let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-        let cur_ver = hklm.open_subkey(r"HKEY_CURRENT_USER\Environment").unwrap();
+        let cur_ver = hklm.open_subkey(r"HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment").unwrap();
 //        let pf: String = cur_ver.get_value("ProgramFilesDir").unwrap();
 //        let dp: String = cur_ver.get_value("DevicePath").unwrap();
 //        println!("ProgramFiles = {}\nDevicePath = {}", pf, dp);
