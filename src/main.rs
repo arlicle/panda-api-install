a6269b5ebfe036f721bdf384c225f6dc06cc2076
edison <arlicle@gmail.com>
2020-02-24 08:07:46 +0800

    fix *

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     #[cfg(windows)]
     {
         println!("Reading some system info...");
-        let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+        let hklm = RegKey::predef(HKEY_CURRENT_USER);
         let cur_ver = hklm.open_subkey(r"HKEY_CURRENT_USER\Environment").unwrap();
 //        let pf: String = cur_ver.get_value("ProgramFilesDir").unwrap();
 //        let dp: String = cur_ver.get_value("DevicePath").unwrap();
